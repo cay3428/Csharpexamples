29e87bea3eda1f594f0f1437d3ca8a8963e049a7
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-21 14:53:38 +0300

    tekçift

--- firstexam/Program.cs
+++ firstexam/Program.cs
@@ -6,13 +6,36 @@ namespace firstexam
     {
         static void Main(string[] args)
         {
-            for (int i = 10; i >= 0; i--)
-            {
-                Console.WriteLine(i);
+            //for (int i = 10; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+ //           for (int i = 1; i <= 100; i++)
+ //           { 
+ //               if  (i % 2 == 0)
+ //               {
+ //                   Console.WriteLine(i+"çift");
+
+ //               }
+ //               else 
+ //{
+ //                   Console.WriteLine(i + "tek");
+
+ //               }
+                     
+                             
             }
 
+          
         }
     }
 }
 //"1. Console ekranına 10'dan geriye yazdıran programı yazınız. 
 //Bilinmesi gereken : for döngüsü"
+//2. Console ekranına 1 den 100'e kadar olan sayıları sıralı olarak çift ise çift, tek ise tek olarak yazdırın.
+//Bilinmesi gereken : for döngüsü, if/else
+//            Ör Çıktı : 
+//1 - Tek
+//2 - Çift
+//3 - Tek vb.
