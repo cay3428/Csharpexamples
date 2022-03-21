832f4c598d5e701c64304b3d033c23e32b513ecb
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-21 15:14:18 +0300

    aylar

--- firstexam/Program.cs
+++ firstexam/Program.cs
@@ -6,31 +6,137 @@ namespace firstexam
     {
         static void Main(string[] args)
         {
+            //string ay;
+           
+            //Console.Write(" 1-ocak  ");
+            //Console.Write(" 2-şubat ");
+            //Console.Write("  3-mart ");
+            //Console.Write("  4-nisan ");
+            //Console.Write(" 5-mayıs ");
+            //Console.Write(" 6-haziran ");
+            //Console.Write(" 7-temmuz ");
+            //Console.Write(" 8-ağustos ");
+            //Console.Write(" 9-eylül ");
+            //Console.Write(" 10-ekim ");
+            //Console.Write(" 11-kasım ");
+            //Console.Write(" 12-aralık ");
+            //Console.Write(" ay ismi giriniz : ");
+            //ay = Console.ReadLine();
+            //switch(ay)
+            //{
+            //    case "ocak" :
+            //        Console.WriteLine("31 gün");
+
+            //            break;
+            //    case "şubat":
+            //        Console.WriteLine("28 gün");
+            //        break;
+            //    case  "mart" :
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+            //    case "nisan":
+            //        Console.WriteLine("30 gün");
+            //        break;
+
+            //    case "mayıs":
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+            //    case "haziran":
+            //        Console.WriteLine("30 gün");
+            //        break;
+
+            //    case "temmuz":
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+            //    case "ağustos":
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+            //    case "eylül":
+            //        Console.WriteLine("30 gün");
+            //        break;
+
+            //    case "ekim":
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+            //    case "kasım":
+            //        Console.WriteLine("30 gün");
+            //        break;
+
+
+            //    case "aralık":
+            //        Console.WriteLine("31 gün");
+            //        break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             //for (int i = 10; i >= 0; i--)
             //{
             //    Console.WriteLine(i);
             //}
 
- //           for (int i = 1; i <= 100; i++)
- //           { 
- //               if  (i % 2 == 0)
- //               {
- //                   Console.WriteLine(i+"çift");
+            //           for (int i = 1; i <= 100; i++)
+            //           { 
+            //               if  (i % 2 == 0)
+            //               {
+            //                   Console.WriteLine(i+"çift");
 
- //               }
- //               else 
- //{
- //                   Console.WriteLine(i + "tek");
+            //               }
+            //               else 
+            //{
+            //                   Console.WriteLine(i + "tek");
 
- //               }
-                     
-                             
-            }
+            //               }
+
+
+        }
 
           
         }
     }
-}
+
 //"1. Console ekranına 10'dan geriye yazdıran programı yazınız. 
 //Bilinmesi gereken : for döngüsü"
 //2. Console ekranına 1 den 100'e kadar olan sayıları sıralı olarak çift ise çift, tek ise tek olarak yazdırın.
@@ -39,3 +145,10 @@ namespace firstexam
 //1 - Tek
 //2 - Çift
 //3 - Tek vb.
+
+
+//3.Console ekranına bu yılın girdiğim bir ayının(Ay ismi veya ay numarası ile yapılabilir) gün sayısını veren program.
+//Bilinmesi gereken : switch case
+//Ör Çıktı:
+//    Ayı seçiniz : Şubat
+//28
